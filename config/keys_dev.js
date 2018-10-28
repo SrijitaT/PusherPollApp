89914c4caf0b04fb571891679d61832fcb68d602
Srijita Thakur <34444499+SrijitaT@users.noreply.github.com>
2018-10-28 15:58:56 +0530

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://srijita:srijita123@srijita-m001-ivj0d.mongodb.net/pusherpoll?retryWrites=true",
-  pusherAppId: "633147",
-  pusherKey: "29e069446f8d6c4be626",
-  pusherSecret: "0f640d4be79f502283c7",
-  pusherCluster: "ap2",
-  pusherEncrypted: true
-};
